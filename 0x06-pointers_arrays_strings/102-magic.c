234c7eeda73b0d158ea3e6f8fc054afe9193137b
dreamer-was-taken <emaore0707@gmail.com>
2023-06-29 21:43:59 +0100

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,30 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int a[5] = {98, 402, 198, 298, 1024};
+	int *p = a;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * Write your line of code here...
-   *
-   * Description: In this section, you are required to write a single line of code
-   *              to accomplish a specific task. There are some limitations and
-   *              restrictions to keep in mind:
-   *              - You are not allowed to use variable 'a'.
-   *              - You are not allowed to modify variable 'p'.
-   *              - Only one statement is permitted.
-   *              - The code you write should be limited to this line only and
-   *                should not include any additional code or statements.
-   *
-   * Task: Write the necessary code to complete the specified task within these
-   *       constraints.
-   */
-  p[5] = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	p[2] = 98;  /* Add this line to set a[2] to 98 */
+
+	printf("a[2] = %d\n", a[2]);
+
+	return 0;
 }
