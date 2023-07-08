07dcd217c9502bb554cb4428f1c5afbcc5852db0
dreamer-was-taken <emaore0707@gmail.com>
2023-07-08 01:55:08 +0100

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-gcc -c *.c
-ar -rc liball.a *.o
+
+# Compile each .c file into object files
+for file in *.c; do
+    gcc -c "$file"
+done
+
+# Create the static library using ar
+ar rcs liball.a *.o
+
+# Clean up the object files
+rm *.o
